8e07bf6a607aaef740ab586762b1f1bb3da15a8f
hiqex <nnmmfftt@163.com>
2019-04-18 21:19:34 +0800

    add 14

---
+++ 1-50/14.Longest Common Prefix.cpp
@@ -0,0 +1,28 @@
+/*--------------------1st--------------------*/
+string longestCommonPrefix(vector<string>& strs) {
+	/*
+	 * First, handle boundary cases.
+	 * And then, check every character. If not matching, go to out.
+	 * In a function, goto is hardly makes mistakes.
+	 * In a project need use goto in its own function body.
+	 */
+    if (strs.size() == 0) return "";
+    if (strs.size() == 1)
+        return strs[0];
+    string ret = "";
+    for (int i = 0; ; ++i) {
+        char tmp = strs[0][i];
+        for (int j = 1; j< strs.size(); ++j) {
+            if (strs[j].size() > i) {
+                if (tmp != strs[j][i])
+                    goto out;
+            }
+            else
+                goto out;
+        }
+        ret += tmp;
+    }
+end:
+    return ret;
+}
+/*--------------------2nd--------------------*/
