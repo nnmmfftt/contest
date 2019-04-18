6210d08479e7c713a5c16b610a2882f26cf70a33
hiqex <nnmmfftt@163.com>
2019-04-18 23:46:30 +0800

    add 14 trie tmp

--- 1-50/14.Longest Common Prefix.cpp
+++ 1-50/14.Longest Common Prefix.cpp
@@ -32,3 +32,61 @@ end:
     return ret;
 }
 /*--------------------2nd--------------------*/
+struct Trie
+{
+	Trie *next[26];
+	int count;
+}trie;
+void insert(string s){
+	Trie *p= root, *tmp;
+	for(int i = 0; i < s.size(); ++i){
+		int t = s[i]-'a';
+		if(p->next[t] == NULL){
+			tmp = new Trie;
+			tmp->count = 1;
+			for(int j = 0; j < 26; ++j){
+				tmp->next[j] = NULL;
+			}
+			p->next = tmp;
+		}
+		else {
+			p->next[t]->count++;
+		}
+	}
+}
+int search(string s,string ret){
+	Trie *p = root;
+	bool find_val = 0;
+	for(int i = 0; i < s.size(); ++i){
+		int t = s[i] -'a';
+		if(p->next[t] == NULL) 
+			return 0;
+		if(p->next[t] -> count == n){
+			ret = s.substr(0,i+1);
+			find_val = 1;
+		}
+		p = p->next[t];
+	}
+	return find_val;
+}
+
+void init(){
+    root = new Trie;
+    root->count = 0;
+    for(int i = 0;i < 26;++ i){
+        root->next[i] = NULL;
+    }
+}
+
+string longestCommonPrefix(vector<string>& strs) {
+	string ret = "";
+	n = strs.size();
+	if (!n) 
+		return ret;
+	init();
+	for (int i = 0;i < n;++ i){
+		insert(strs[i]);
+	}
+	if (!search(strs[0],ret)) return ans = "";
+  return ans;
+}
