2ed1bea11761121efbfb009b8a2b5144f98d5b78
hiqex <nnmmfftt@163.com>
2019-04-29 20:25:16 +0800

    Update 51.N-Queens.cpp

--- LC/51-100/51.N-Queens.cpp
+++ LC/51-100/51.N-Queens.cpp
@@ -1,14 +1,60 @@
+/*--------------------1st--------------------*/
 class Solution {
 public:
-	int check_position(int *is_placed, int row);
-	int draw_graph(vector<vector<string>> &ret, int *is_placed, int n);
+	vector<vector<string>> solveNQueens(int n){
+		/*
+		 * Check the every position always is a repetitive process.
+		 * Then use recursion to solve the problem.
+		 *
+		 * The recursion steps is check every position, if a chess is put down, use dfs and
+		 * check the remain positon.
+		 */
+		vector<vector<string>> ret;
+		vector<string> graph(n, string(n, '.'));
+		nQueens_dfs(0, n, graph, ret);
+		return ret;
+	}
+
+private:
+	void nQueens_dfs(int row, int n, vector<string> &graph, vector<vector<string>> &ret){
+		if(row == n ){
+			ret.push_back(graph);
+			return;
+		}
+		for(int i = 0; i < n; ++i){
+			if(is_placed(graph, n, row, i)){
+				graph[row][i] = 'Q';
+				nQueens_dfs(row + 1, n, graph, ret);
+				graph[row][i] = '.';
+			}
+		}
+	}
+
+	bool is_placed(vector<string> &graph, int n, int row, int col){
+		for(int i = 0; i < row; ++i){
+			if(graph[i][col] == 'Q') return false;
+			if(col + row - i < n && graph[i][col + row - i] == 'Q') return false;
+			if(col + row + i >= 0 && graph[i][col - row + i] == 'Q') return false; 
+		}
+		return true;
+	}
+};
+
+/*--------------------2nd--------------------*/
+class Solution {
+public:
     vector<vector<string>> solveNQueens(int n) {
+    	/*
+    	 * If not use recursion, the find operation need to reset the postion flags if cannot 
+    	 * put down the all given chess pieces.
+    	 */
         vector<vector<string>> ret;
         int is_placed[n];
         memset(is_placed, -1, sizeof(is_placed));
 
         int row = 0;
         while(row >= 0){
+        	is_placed[row]++;
         	while( is_placed[row] < n && !check_position(is_placed, row))
         		++ is_placed[row];
         	if(is_placed[row] < n){
@@ -27,8 +73,8 @@ public:
         }return ret;
     }
     int check_position(int *is_placed, int row){
-    	for(int i = 0; i < ro; ++i){
-    		if(is_placed[i] == is_placed[row] || abs(is_placed[i] = is_placed[row]) == row-i ){
+    	for(int i = 0; i < row; ++i){
+    		if(is_placed[i] == is_placed[row] || abs(is_placed[i] - is_placed[row]) == row-i ){
     			return false;
     		}
     	}return true;
