a6d744fe0d3d5e7d8c2392c185e4d85678959235
hiqex <nnmmfftt@163.com>
2019-05-28 17:12:44 +0800

    Update 32.Longest Valid Parentheses.cpp

--- LC/1-50/32.Longest Valid Parentheses.cpp
+++ LC/1-50/32.Longest Valid Parentheses.cpp
@@ -17,35 +17,3 @@ public:
         return ret;
     }
 };
-class Solution {
-public:
-    int longestValidParentheses(string s) {
-        if(0 == s.size()) return 0;
-        stack<char> match;
-        vector<char> tmp;
-        for(auto i : s){
-            if(match.size()!=0 && i ==')'){
-                tmp.push_back(match.top());
-                match.pop();
-                tmp.push_back(i);
-            }
-            if(i == '('){
-                match.push(i);
-            }
-            
-        }
-        sort(tmp.begin(),tmp.end());
-        int pos = 0;
-        int ret = 0;
-        int len = tmp.size();
-        while(pos < len){
-            int j = pos;
-            while(j < len -1 && tmp[j+1] == tmp[j] +1){
-                j+=1;
-            }
-            ret = ret > (j-pos+1)? ret:(j-pos+1);
-            pos = j+1;
-        }
-        return ret;
-    }
-};
